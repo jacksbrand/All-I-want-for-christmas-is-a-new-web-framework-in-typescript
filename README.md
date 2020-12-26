3512a2e377e46738d0f4acd8f5193ad12d939ce5
Jack <jack@pop-os.localdomain>
2020-12-26 13:26:00 +0000

    adding strict type checking and setAge/setName functions

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A simple front end framework for handing typical models. Models contain 'events', 'attriutes' and 'sync' properties.
 
 User model has been implemented, but the code should be dsigned to be as reusable as is reasonable.
+
+ADD DEV INSTRUCTIONS
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,18 +1,33 @@
+import { User } from "../models/User";
+
 export class UserForm {
-  constructor(public parent: Element) {}
-
-  onButtonClick(): void {
-    console.log("Clicked Me!");
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
   }
 
-  onHeaderMouseOver(): void {
-    console.log("Hovering!!");
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
   }
+
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
+
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
-      "mouseover:h1": this.onHeaderMouseOver,
+      "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
     };
   }
 
@@ -32,13 +47,18 @@ export class UserForm {
     return `
         <div>
             <h1>User Form</h1>
+            <div>Name: ${this.model.get("name")}</div>
+            <div>Age: ${this.model.get("age")}</div>
             <input />
-            <button>Click Me</button>
+            <button class='set-name'>Change Name</button>
+            <button class="set-age">Set Random Age</button>
         </div>
     `;
   }
 
   render(): void {
+    this.parent.innerHTML = "";
+
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
