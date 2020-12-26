f27e82b1b2400a4068bc589298a54551d2a7d622
Jack <jack@pop-os.localdomain>
2020-12-26 23:06:01 +0000

    Factoring out the renderer as an abstract inheritance

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,16 +1,6 @@
 import { User } from "../models/User";
-
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel(): void {
-    this.model.on("change", () => {
-      this.render();
-    });
-  }
-
+import { View } from "./View";
+export class UserForm extends View {
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
   };
@@ -31,18 +21,6 @@ export class UserForm {
     };
   }
 
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
   template(): string {
     return `
         <div>
@@ -54,16 +32,5 @@ export class UserForm {
             <button class="set-age">Set Random Age</button>
         </div>
     `;
-  }
-
-  render(): void {
-    this.parent.innerHTML = "";
-
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
   }
 }
